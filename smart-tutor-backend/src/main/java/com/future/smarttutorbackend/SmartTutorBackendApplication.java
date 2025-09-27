54330ee9c22ed3a03e8aeef96c682a6e2f94c9b8
ahmed <Ahmed_20230044@fci.helwan.edu.eg>
2025-09-27 15:52:28 +0300

    test

---
+++ smart-tutor-backend/src/main/java/com/future/smarttutorbackend/SmartTutorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.future.smarttutorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTutorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTutorBackendApplication.class, args);
+    }
+
+}
---
+++ smart-tutor-backend/src/test/java/com/future/smarttutorbackend/SmartTutorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.smarttutorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTutorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
